de934ce8090e4eb93ae712be441c606faece58e0
davsi111 <davsi111@in.ibm.com>
2021-05-14 10:53:39 +0530

    OHAI

---
+++ Task8-OHAI/cookbooks/workstation/recipes/setup.rb
@@ -0,0 +1,15 @@
+package 'chrony' do
+    action :install
+end
+package 'nano' do
+    action :install
+end
+file '/etc/motd' do
+    content "Task8 started for OHAI
+    IPADDRESS: #{node['hostname']}
+    HOSTNAME: #{node['ipaddress']}
+    CPU: #{node['cpu']['0']['mhz']}
+    action :create
+end
+
+
