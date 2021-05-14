c13c9ee8ff0a8261de1d6b7ef175ac6ff7494b6c
davsi111 <davsi111@in.ibm.com>
2021-05-14 10:08:13 +0530

    task7 recipe

---
+++ Task7-default-recipe/cookbooks/workstation/recipes/server.rb
@@ -0,0 +1,19 @@
+package 'httpd' do
+    action :remove
+end
+
+package 'httpd' do
+    action :install
+end
+service 'httpd' do
+    action [:enable, :start]
+end
+file '/var/www/html/index.html' do
+    content 'Webserver to create new recipe for Task7'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+
