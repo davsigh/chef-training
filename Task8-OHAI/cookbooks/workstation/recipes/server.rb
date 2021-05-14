7f1ba0c926a0742476ae8bd350e89bf00bcec68b
davsi111 <davsi111@in.ibm.com>
2021-05-14 11:23:49 +0530

    OHAI apache

---
+++ Task8-OHAI/cookbooks/workstation/recipes/server.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook:: workstation
+# Recipe:: server.rb
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+package 'httpd' do
+    action :remove
+end
+package 'httpd' do
+    action :install
+end
+file '/var/www/html/index.html' do
+    content "<h1>Its my webserver</h1>
+    \nIPADDRESS: #{node[ipaddress]}
+    \nHOSTNAME: #{node[hostname]}"
+    action :create
+end
+
+
