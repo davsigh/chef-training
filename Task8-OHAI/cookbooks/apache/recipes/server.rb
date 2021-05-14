ac3be03519ee7fd74f40b55bc0af79ca12a813c4
davsi111 <davsi111@in.ibm.com>
2021-05-14 12:01:51 +0530

    test

--- Task8-OHAI/cookbooks/apache/recipes/server.rb
+++ Task8-OHAI/cookbooks/apache/recipes/server.rb
@@ -11,8 +11,8 @@ package 'httpd' do
 end
 file '/var/www/html/index.html' do
     content "<h1>Its my webserver</h1>
-    <h2> \nIPADDRESS: #{node['ipaddress']}\n </h2>
-    <h2>\nHOSTNAME: #{node['hostname']}</h2>"
+    <h2> IPADDRESS: #{node['ipaddress']} </h2>
+    <h2>HOSTNAME: #{node['hostname']}</h2>"
     action :create
 end
 service 'httpd' do
