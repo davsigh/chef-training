384ec7e4ac5adc5416300eb05b3f1b9149a85feb
davsi111 <davsi111@in.ibm.com>
2021-05-17 01:32:03 +0530

    new

--- chef-repo/cookbooks/workstation/recipes/server.rb
+++ chef-repo/cookbooks/workstation/recipes/server.rb
@@ -1,19 +1,4 @@
-package 'httpd' do
-    action :remove
-end
-package 'httpd' do
-    action :install
-end
-template '/var/www/html/index.html' do
-    source 'Index.html.erb'
-    owner 'root'
-    group 'root'
-    mode '0755'
-    action :create
-end
-service 'httpd' do
-    action :start
-end
+
 template '/etc/motd' do
     source 'motd.erb'
        action :delete
